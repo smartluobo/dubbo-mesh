145d62a3478d9e674fe11f84f4574156fdc02c56
smartluobo <1043254182@qq.com>
2018-06-07 23:59:49 +0800

    clear tomcat

--- mesh-agent/src/main/java/com/smartluobo/mesh/agent/controller/HelloController.java
+++ mesh-agent/src/main/java/com/smartluobo/mesh/agent/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.smartluobo.mesh.agent.controller;
 
 import com.smartluobo.mesh.agent.service.HelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ import javax.annotation.Resource;
 @RestController
 public class HelloController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @Resource
     private HelloService helloService;
 
@@ -18,6 +22,10 @@ public class HelloController {
                          @RequestParam("method") String method,
                          @RequestParam("parameterTypesString") String parameterTypesString,
                          @RequestParam("parameter") String parameter) throws Exception{
-        return helloService.invoke(interfaceName,method,parameterTypesString,parameter);
+        long startTime = System.currentTimeMillis();
+        Object invoke = helloService.invoke(interfaceName, method, parameterTypesString, parameter);
+        long endTime = System.currentTimeMillis();
+        LOGGER.info("*********HelloController ...consumer-agent to provider-agent wait time: "+(endTime-startTime)+"ms");
+        return invoke;
     }
 }
